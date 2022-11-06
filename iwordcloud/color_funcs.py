04be9032642010b55b82bb1c4aecd007888db0c7
Jake Brehm <jake.m.brehm@gmail.com>
2022-11-06 10:38:33 -0500

    Updated requirements and began creating the word cloud

--- iwordcloud/color_funcs.py
+++ iwordcloud/color_funcs.py
@@ -1,9 +1,34 @@
 import random
 
 
+def random_color_func(word=None, font_size=None, position=None,  orientation=None, font_path=None, random_state=None):
+    h = int(360.0 * 187.0 / 255.0)
+    # h = hue
+    s = int(100.0 * 255.0 / 255.0)
+    l = int(100.0 * float(random_state.randint(60, 120)) / 255.0)
+
+    return f"hsl({h}, {s}%, {l}%)"
+
+
 def random_gray(
     word, font_size, position, orientation, random_state=None, **kwargs
 ):
     """Randomly generates a gray hsl color."""
 
     return "hsl(0, 0%%, %d%%)" % random.randint(60, 100)
+
+
+def constant_hue(
+    hue
+):
+    """"""
+
+    def _constant_hue(word=None, font_size=None, position=None,  orientation=None, font_path=None, random_state=None):
+        """"""
+        h = hue
+        s = int(100.0 * 255.0 / 255.0)
+        l = int(100.0 * float(random_state.randint(60, 120)) / 255.0)
+
+        return f"hsl({h}, {s}%, {l}%)"
+
+    return _constant_hue
--- main.py
+++ main.py
@@ -1,37 +1,49 @@
 import iwordcloud as iwc
 
+
 if __name__ == '__main__':
+    # Specify input and output paths
     input_path = r'data/texts.txt'
     output_path = r'data/output.txt'
 
+    # Create the iMessages object
     imessages = iwc.iMessages(input_path, own_name='Jake Brehm')
-    # imessages.get_all()
 
-    # print(imessages.get_sent())
-    # print(imessages.sent)
-    
-    # imessages.trim('2022-07-02 16:00:00', '2022-07-02 23:59:59')
-    # imessages.trim('2022-07-02 00:00:00', '2022-07-02 23:59:59')
-    # imessages.trim('2022-07-02 16:00:00', '2022-07-02 18:00:00')
-    # print(imessages.sent)
-
-    # for reaction in imessages.reactions:
-    #     print(reaction.name)
-
-    # print(imessages.reactions.get_count())
-    # print(imessages.reactions.get_count('Loved'))
-    # print(imessages.reactions.get_messages('Disliked'))
-
-    # imessages.save_all(output_path)
-
+    # Create the WordCloud object and set parameters
     cloud = iwc.MessageCloud(imessages)
-    cloud.background_color = (0, 105, 148)
+
+    # Settings for the word cloud generation for easy changes
+    CONTOUR = False
+
+    # Set the word cloud's background color
+    # cloud.background_color = (85, 219, 237) # Hannah's favorite color
+    cloud.background_color = (255, 255, 255)
+
+    # Specify the font and related settings to be used for the word cloud
+    # cloud.font_path = 'DIN Condensed Bold'
+    cloud.font_path = 'RobotoCondensed-Regular'
+    # cloud.font_path = 'Bell MT'
+    cloud.min_font_size = 4
+    cloud.max_font_size = 96
+
+    # Set the custom mask for the word cloud and well as its contour
     cloud.mask = 'data/manatee_small_blur.png'
-    cloud.contour_width = 3
-    cloud.contour_color = 'gray'
-    cloud.margin = 10
+    if CONTOUR:
+        cloud.contour_width = 2
+        cloud.contour_color = (85, 219, 237)
+        cloud.margin = 5
+
+    # Define the number of words to be shown in the word cloud
+    cloud.max_words = 400
+
+    # Word processing settings
+    cloud.min_word_length = 2
+    cloud.collocations = False          # will not include pairs of words
+    cloud.include_numbers = False       # will not include numbers
+
+    # Generate the word cloud and recolor
     cloud.generate()
-    cloud.recolor(color_func=iwc.color_funcs.random_gray, random_state=3)
+    cloud.recolor(color_func=iwc.color_funcs.constant_hue(187), random_state=3)
+
+    # Save the word cloud to a file
     cloud.save('data/output.png')
-    print(cloud.words.get_counts())
-    print(cloud.words.get_most_frequent(20))
