c4692b27b42dd3ae4e84320d472594b07d627c22
Jake Brehm <jake.m.brehm@gmail.com>
2022-12-27 14:01:31 -0500

    Refactored the sentiment module

--- demesstify/analysis/sentiment.py
+++ demesstify/analysis/sentiment.py
@@ -9,43 +9,30 @@ Provides functionality for performing sentiment analysis.
 import pandas as pd
 from vaderSentiment.vaderSentiment import SentimentIntensityAnalyzer
 
-from ..parse import Messages
+from ..parse import Direction, Messages
 
 
-class SentimentAnalyzer(SentimentIntensityAnalyzer):
+class Sentiment(SentimentIntensityAnalyzer):
     """Performs sentiment analysis on message data."""
 
-    def __init__(self, messages: Messages, *args, **kwargs):
-        """Inits the SentimentAnalyzer instance."""
+    def __init__(self, data: pd.DataFrame, *args, **kwargs):
+        """Initializes the Sentiment instance."""
 
         # Init the parent class
         super().__init__(*args, **kwargs)
 
         # Store instance variables
-        self._messages = messages
+        self._data = data
     
-    def get_average_sentiment(self, whom: str="all"):
-        """"""
-
-        # Extract relevant data from the Messages object
-        if whom == 'all':
-            data = self._messages.all.get()
-        elif whom == 'sender':
-            data = self._messages.sent.get()
-        elif whom == 'recipient':
-            data = self._messages.received.get()
-        else:
-            raise ValueError(f"'{whom}' is not a valid value for 'whom'.")
+    def get_average_sentiment(self):
+        """Gets the average sentiment of the data."""
         
         # Generate pandas series of compound polarity scores
         polarities = pd.Series(
-            data['message'].apply(
+            self._data['message'].apply(
                 lambda text: self.polarity_scores(text)['compound']
             )
         )
 
         # Return the average polarity
         return polarities.mean()
-        
-
-        
